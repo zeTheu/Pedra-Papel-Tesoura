5af364a83093eceeb801f0d67867df14ea84c9bb
Matheus Reis <matheuzim011@hotmail.com>
2022-10-07 14:00:21 -0300

    Adicionado funçoes e melhorado a lógica

--- PePaTe.py
+++ PePaTe.py
@@ -1,70 +1,84 @@
+#BIBLIOTECAS IMPORTADAS
 import random
 import time
 
-#APRESENTAÇÃO DO JOGO
-print('------------JO KEN PO------------')
+#O JOGO
+def jogar():
 
-print('\n[ 1 ]PEDRA \n[ 2 ]PAPEL \n[ 3 ]TESOURA')
+#APRESENTAÇÃO DO JOGO
+    print('------------JO KEN PO------------')
 
 #ESCOLHAS DA MÁQUINA E DO JOGADOR
-escolha = int(input('\nQual você escolhe? '))
-maquina = random.randrange(0, 4)
-opcao = ['Pedra', 'Papel', 'Tesoura']
+    escolha = escolher()
+    maquina = random.randrange(1, 4)
+    opcao = ['Pedra', 'Papel', 'Tesoura']
 
-continuar = False
-rejogar = 2   
-
-#BLOCO DE ANDAMENTO DO JOGO
-def jogada(mostra_opcao):
-            
-        print('Jogando... \n')
-        time.sleep(1)
-        print('JO')
-        time.sleep(1)
-        print('KEN')
-        time.sleep(1)
-        print('PO \n')
-        time.sleep(1)
-        if escolha == maquina:
-            if escolha == 1:
-                palavra = 'Pedra'
-            elif escolha == 2:
-                palavra = 'Papel'
-            elif escolha == 3:
-                palavra = 'Tesoura'
-            print('Vocês escolheram {}'.format(palavra))
-            print('JOGO EMPATADO')
-        
-        elif escolha == 1 and maquina == 2:
-            print('Você escolheu {} e seu adversário escolheu {}'.format(mostra_opcao[0], mostra_opcao[1]))
-            print('Papel embrulha pedra. \nVOCÊ PERDEU!')
-        elif escolha == 1 and maquina == 3:
-            print('Você escolheu {} e seu adversário escolheu {}'.format(mostra_opcao[0], mostra_opcao[2]))
-            print('Pedra esmaga tesoura. \nVOCÊ GANHOU!')
-        elif escolha == 2 and maquina == 1:
-            print('Você escolheu {} e seu adversário escolheu {}'.format(mostra_opcao[1], mostra_opcao[0]))
-            print('Papel embrulha pedra. \nVOCÊ GANHOU!')
-        elif escolha == 2 and maquina == 3:
-            print('Você escolheu {} e seu adversário escolheu {}'.format(mostra_opcao[1], mostra_opcao[2]))
-            print('Tesoura corta papel. \nVOCÊ PERDEU!')
-        elif escolha == 3 and maquina == 1:
-            print('Você escolheu {} e seu adversário escolheu {}'.format(mostra_opcao[2], mostra_opcao[0]))
-            print('Pedra esmaga tesoura. \nVOCÊ PERDEU!')
-        elif escolha == 3 and maquina == 2:
-            print('Você escolheu {} e seu adversário escolheu {}'.format(mostra_opcao[2], mostra_opcao[1]))
-            print('Tesoura corta papel. \nVOCÊ GANHOU!')
-        
-jogada(opcao) 
+#BLOCO DE ANDAMENTO DO JOGO      
+    contagem()   
+    jogada(opcao, escolha, maquina)
 
 #REPERGUNTAR SE QUER REPETIR
-while not continuar:
+    repetir()
+
+
+
+
+#FUNÇÕES
+def escolher():
+    print('\n[ 1 ]PEDRA \n[ 2 ]PAPEL \n[ 3 ]TESOURA')
+    escolha = int(input('\nQual você escolhe? '))
+    return escolha
+    
+def contagem():
+    print('Jogando... \n')
+    time.sleep(1)
+    print('JO')
+    time.sleep(1)
+    print('KEN')
+    time.sleep(1)
+    print('PO \n')
+    time.sleep(1)
+
+def jogada(mostra_opcao, escolha, maquina):
+    if escolha == maquina:
+        if escolha == 1:
+            palavra = 'Pedra'
+        elif escolha == 2:
+            palavra = 'Papel'
+        elif escolha == 3:
+            palavra = 'Tesoura'
+        print('Vocês escolheram {}'.format(palavra))
+        print('JOGO EMPATADO')
+    elif escolha == 1 and maquina == 2:
+        print('Você escolheu {} e seu adversário escolheu {}'.format(mostra_opcao[0], mostra_opcao[1]))
+        print('Papel embrulha pedra. \nVOCÊ PERDEU!')
+    elif escolha == 1 and maquina == 3:
+        print('Você escolheu {} e seu adversário escolheu {}'.format(mostra_opcao[0], mostra_opcao[2]))
+        print('Pedra esmaga tesoura. \nVOCÊ GANHOU!')
+    elif escolha == 2 and maquina == 1:
+        print('Você escolheu {} e seu adversário escolheu {}'.format(mostra_opcao[1], mostra_opcao[0]))
+        print('Papel embrulha pedra. \nVOCÊ GANHOU!')
+    elif escolha == 2 and maquina == 3:
+        print('Você escolheu {} e seu adversário escolheu {}'.format(mostra_opcao[1], mostra_opcao[2]))
+        print('Tesoura corta papel. \nVOCÊ PERDEU!')
+    elif escolha == 3 and maquina == 1:
+        print('Você escolheu {} e seu adversário escolheu {}'.format(mostra_opcao[2], mostra_opcao[0]))
+        print('Pedra esmaga tesoura. \nVOCÊ PERDEU!')
+    elif escolha == 3 and maquina == 2:
+        print('Você escolheu {} e seu adversário escolheu {}'.format(mostra_opcao[2], mostra_opcao[1]))
+        print('Tesoura corta papel. \nVOCÊ GANHOU!')
+
+
+def repetir():
     rejogar = int(input('\nVocê quer jogar novamente? \n1 = SIM \n2 = NÃO \n-> '))
     if rejogar == 2:
-        continuar = True
         print('\nJOGO FINALIZADO!')
+        exit()
     else:
-        print('\n[ 1 ]PEDRA \n[ 2 ]PAPEL \n[ 3 ]TESOURA')
-        escolha = int(input('\nQual você escolhe? '))
-        jogada(opcao)
-        
+        jogar()
+
+
+if __name__ == "__main__":
+    jogar()
+
 #PEPATES
